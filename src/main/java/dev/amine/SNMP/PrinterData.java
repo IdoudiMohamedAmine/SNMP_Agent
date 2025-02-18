09896157168c4a63892982f9f0631fa84665625f
IdoudiMohamedAmine <mohamedamineidoudi@gmail.com>
2025-02-18 15:05:15 +0100

    lombook is here

--- src/main/java/dev/amine/SNMP/PrinterData.java
+++ src/main/java/dev/amine/SNMP/PrinterData.java
@@ -1,8 +1,13 @@
 package dev.amine.SNMP;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class PrinterData {
     private String ipAddress;
     private String macAddress;
@@ -10,46 +15,6 @@ public class PrinterData {
     private long totalPages;
     private final Map<String, Integer> tonerLevels=new HashMap<>();
     private final Map<String,String> additionalData=new HashMap<>();
-
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-    public void setIpAddress(String ipAddress) {
-        this.ipAddress = ipAddress;
-    }
-
-    public String getMacAddress() {
-        return macAddress;
-    }
-
-    public void setMacAddress(String macAddress) {
-        this.macAddress = macAddress;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public long getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(long totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public Map<String, Integer> getTonerLevels() {
-        return tonerLevels;
-    }
-
-    public Map<String, String> getAdditionalData() {
-        return additionalData;
-    }
 
     @Override
     public String toString(){
